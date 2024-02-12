f88228ae022a0559365f5dd4343ba44d5b039ff4
Levelin1 <louiskerry.k.guillaume@gmail.com>
2024-02-12 12:33:07 +0100

    Select.js

---
+++ js/select_image.js
@@ -0,0 +1,12 @@
+(() => {
+    fetch("https://pixabay.com/api/?key=37961660-96e6e2efde82126fbcb9618e0&q=yellow+flowers&image_type=photo&pretty=true", {
+        method: "GET"
+    }).then(res => res.json())
+        .then(image => {
+            console.log(image);
+            const img = image.hits[0].largeImageURL;
+            const bg = document.querySelector(".essaie");
+            bg.style.backgroundImage = `url(${img})`
+        });
+}
+)();
